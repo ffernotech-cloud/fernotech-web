981c5392fe192b6992b5771e820b7fb7a628b093
ffernotech-cloud <ffernotech@gmail.com>
2026-05-15 20:37:19 +0200

    Fix duplicate code error

--- src/context/LanguageContext.tsx
+++ src/context/LanguageContext.tsx
@@ -161,28 +161,6 @@ export const LanguageProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-interface LanguageContextType {
-  language: Language;
-  setLanguage: (lang: Language) => void;
-  t: (key: string) => string;
-}
-
-const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
-
-export const LanguageProvider = ({ children }: { children: ReactNode }) => {
-  const [language, setLanguage] = useState<Language>("fr");
-
-  const t = (key: string) => {
-    return translations[key]?.[language] || key;
-  };
-
-  return (
-    <LanguageContext.Provider value={{ language, setLanguage, t }}>
-      {children}
-    </LanguageContext.Provider>
-  );
-};
-
 export const useLanguage = () => {
   const context = useContext(LanguageContext);
   if (!context) throw new Error("useLanguage must be used within a LanguageProvider");
